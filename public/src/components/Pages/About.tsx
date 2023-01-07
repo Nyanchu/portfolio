c69b1bd53fe9550453b257bfbdb7c5e8f72f2fe9
Nyanchu <led.zeppelin.ts@gmail.com>
2023-01-07 21:14:54 +0900

    APIのURLを環境変数に切り出し

--- public/src/components/Pages/About.tsx
+++ public/src/components/Pages/About.tsx
@@ -13,6 +13,53 @@ type User = {
     address: string;
 };
 
+const pagePerCount = 50;
+
+export const About = () => {
+    const [users, setUsers] = useState([] as User[]);
+    const [usersCount, setusersCount] = useState(50 as number);
+    const [page, setPage] = useState(1 as number);
+
+    // リストの生成
+    useEffect(() => {
+        // useEffect自体ではasyncの関数を受け取れないので内部で関数を定義して呼び出す。
+        // TODO: react-query で書き直す？しかし親要素にも影響があるのが直感的ではない
+        (async () => {
+            const offset = (page - 1) * pagePerCount;
+            const response = await fetch(
+                process.env.REACT_APP_URL_FUNCTIONS_API + "/users"
+                + "?limit=" + pagePerCount
+                + "&offset=" + offset
+            );
+            const body = await response.json();
+            setUsers(body.data);
+            setusersCount(body.count);
+        })();
+    }, [page]); // pageの更新があったときに実行
+
+    // ページネーションの生成
+    const pageCount = Math.floor(usersCount / pagePerCount);
+    const handleChange = (page: number) => {console.log(page); setPage(page);};
+
+    return (
+        <div className='main'>
+            <Pagination pageCount={pageCount} page={page} onChange={handleChange} />
+            <BlockList users={users} />
+            <Pagination pageCount={pageCount} page={page} onChange={handleChange} />
+        </div >
+    );
+};
+
+function BlockList({users}: {users: User[];}) {
+    let list: any = [];
+    users.map((user) => list.push(<List user={user} />));
+    return (
+        <ul className='block_list'>
+            {list}
+        </ul>
+    );
+}
+
 function List({user}: {user: User;}) {
     return (
         <li key={user.name}>{/* IDをキーにすべき */}
@@ -30,7 +77,7 @@ function Pagination(
     for (let i = 1; i <= pageCount; i++) {
         list.push(
             <li>
-                <a
+                <a href={"#about/" + i}
                     onClick={() => onChange(i)}
                     className={page === i ? "selected" : ""}
                 >{i}</a>
@@ -43,39 +90,3 @@ function Pagination(
         </ul>
     );
 }
-
-export const About = () => {
-    const [users, setUsers] = useState([] as User[]);
-    const [usersCount, setusersCount] = useState(50 as number);
-    const [page, setPage] = useState(1 as number);
-    const pagePerCount = 50;
-
-    // リストの生成
-    useEffect(() => {
-        // useEffect自体ではasyncの関数を受け取れないので内部で関数を定義して呼び出す。
-        // TODO: react-query で書き直す？しかし親要素にも影響があるのが直感的ではない
-        (async () => {
-            const offset = (page - 1) * pagePerCount;
-            const response = await fetch(
-                "http://127.0.0.1:5001/portfolio-8cf27/us-central1/api/users?"
-                + "limit=" + pagePerCount
-                + "offset=" + offset
-            );
-            const body = await response.json();
-            setUsers(body.data);
-            setusersCount(body.count);
-        })();
-    }, []);
-    const list = users.map((user) => <List user={user} />);
-
-    // ページネーションの生成
-    const pageCount = Math.floor(usersCount / pagePerCount);
-    const handleChange = (page: number) => {console.log(page); setPage(page);};
-
-    return (
-        <div className='main'>
-            <ul className='block_list'>{list}</ul>
-            <Pagination pageCount={pageCount} page={page} onChange={handleChange} />
-        </div >
-    );
-};
